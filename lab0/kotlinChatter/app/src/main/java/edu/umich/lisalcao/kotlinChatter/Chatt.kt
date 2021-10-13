3f54289bf77395aec85712316db213cbb556a86e
lisalcao <46034325+lisalcao@users.noreply.github.com>
2021-10-12 21:07:26 -0400

    set up lab2 but I need to remake the backend

---
+++ lab0/kotlinChatter/app/src/main/java/edu/umich/lisalcao/kotlinChatter/Chatt.kt
@@ -0,0 +1,5 @@
+package edu.umich.lisalcao.kotlinChatter
+
+class Chatt(var username: String? = null,
+            var message: String? = null,
+            var timestamp: String? = null)
---
+++ lab0/kotlinChatter/app/src/main/java/edu/umich/lisalcao/kotlinChatter/ChattListAdapter.kt
@@ -0,0 +1,32 @@
+package edu.umich.lisalcao.kotlinChatter
+
+import android.content.Context
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import edu.umich.lisalcao.kotlinChatter.databinding.ListitemChattBinding
+
+class ChattListAdapter(context: Context, users: ArrayList<Chatt?>) :
+    ArrayAdapter<Chatt?>(context, 0, users) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val listItemView = (convertView?.tag /* reuse binding */ ?: run {
+            val rowView = LayoutInflater.from(context).inflate(R.layout.listitem_chatt, parent, false)
+            rowView.tag = ListitemChattBinding.bind(rowView) // cache binding
+            // ListitemChattBinding.inflate(LayoutInflater.from(context), parent, false).root
+            // or no need for .root even?
+            rowView.tag
+        }) as ListitemChattBinding
+
+        getItem(position)?.run {
+            listItemView.usernameTextView.text = username
+            listItemView.messageTextView.text = message
+            listItemView.timestampTextView.text = timestamp
+            listItemView.root.setBackgroundColor(Color.parseColor(if (position % 2 == 0) "#E0E0E0" else "#EEEEEE"))
+        }
+
+        return listItemView.root
+    }
+}
